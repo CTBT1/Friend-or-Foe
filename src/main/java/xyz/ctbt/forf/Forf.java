784f9d0d611d882629cce9eb2db63228715d9df2
CTBT <funnylookingdude1@gmail.com>
2025-07-19 13:12:36 -0700

    Initial commit

---
+++ src/main/java/xyz/ctbt/forf/Forf.java
@@ -0,0 +1,18 @@
+package xyz.ctbt.forf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Forf extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        //first commit
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
